c6f9731722d9e0bd2fea6551cf12b2681d39aa4a
WindAsMe <542636539@qq.com>
2018-06-02 13:53:24 +0800

    Python For LeeCode

---
+++ venv/include/Python_Daily_5/increasingTriplet.py
@@ -0,0 +1,34 @@
+# !/usr/bin/python3
+# -- coding: UTF-8 --
+# Author   :WindAsMe
+# Date     :18-6-2 下午1:23
+# File     :increasingTriplet.py
+# Location:/Home/PycharmProjects/..
+
+# LeeCode No.334
+
+
+class Solution:
+    @staticmethod
+    def increasingTriplet(nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if len(nums) <= 3:
+            return False
+        else:
+            p1 = int("INF")
+            p2 = int("INF")
+            for i in range(0, len(nums)):
+                if nums[i] <= p1:
+                    p1 = nums[i]
+                elif nums[i] <= p2:
+                    p2 = nums[i]
+                else:
+                    return True
+            return False
+
+
+if __name__ == '__main__':
+    print(Solution.increasingTriplet([2,1,5,0,4,6]))
